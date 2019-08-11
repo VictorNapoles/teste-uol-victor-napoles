f5fc88ba548b353ddeb748f89974c79590934199
Victor de Nápoles <victor.napoles-impulso@m4u.com.br>
2019-08-11 16:53:06 -0300

    Commit incial

---
+++ src/main/java/br/com/uol/testeuolvictornapoles/TesteUolVictorNapolesApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol.testeuolvictornapoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUolVictorNapolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUolVictorNapolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uol/testeuolvictornapoles/TesteUolVictorNapolesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol.testeuolvictornapoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUolVictorNapolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
