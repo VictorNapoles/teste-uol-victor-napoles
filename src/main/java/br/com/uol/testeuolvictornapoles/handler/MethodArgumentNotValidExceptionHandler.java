312ad19c0bf79b03352a22a84c27b258ca7a2ffc
Victor de Nápoles <victor.napoles-impulso@m4u.com.br>
2019-08-17 19:20:54 -0300

    Criação de handler.

---
+++ src/main/java/br/com/uol/testeuolvictornapoles/handler/MethodArgumentNotValidExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.com.uol.testeuolvictornapoles.handler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.uol.testeuolvictornapoles.handler.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class MethodArgumentNotValidExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<ObjectError> errors = getErrors(ex);
+        ErrorResponse errorResponse = getErrorResponse(ex, status, errors);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    private ErrorResponse getErrorResponse(MethodArgumentNotValidException ex, HttpStatus status, List<ObjectError> errors) {
+        return new ErrorResponse("Requisição possui campos inválidos", status.value(),
+                status.getReasonPhrase(), ex.getBindingResult().getObjectName(), errors);
+    }
+
+    private List<ObjectError> getErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new ObjectError(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+    }
+}
